aa8f937633c72ffd10110971fe4073edc455f3d7
Petter1307 <petrealupu75@gmail.com>
2021-12-30 22:50:38 +0200

    Adding gitignore

--- main.cpp
+++ main.cpp
@@ -6,39 +6,7 @@ using namespace std;
 
 int main()
 {
-    cup cup1;
-    cup1.set_weight(120);
-    cup1.set_color("Red");
-    cup1.set_utilization("Tea");
-    cup1.set_volume(200);
-
-    cup cup2("Blue", "Coffe", 300, 100);
-
-    cup cup3(cup1);
-
-    if (cup1 >= cup3)
-        cout << "Yes" << endl;
-    else
-        cout << "No" << endl;
-
-    if (cup3 <= cup3)
-        cout << "Yes" << endl;
-    else
-        cout << "No" << endl;
-
-    if (cup1 > cup2)
-        cout << "Yes" << endl;
-    else
-        cout << "No" << endl;
-
-    if (cup1 < cup2)
-        cout << "Yes" << endl;
-    else
-        cout << "No" << endl;
-
-    cup1.show_cup_details();
-    cup2.show_cup_details();
-    cup3.show_cup_details();
-
+    int a[] = {1, 2, 3, 5};
+    cout << sizeof(a) / sizeof(int);
     return 0;
 }
